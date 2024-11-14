2447d84437e9ee112c0b788cbb6961bbe8bceb48
lf0rlance <163134925+lf0rlance@users.noreply.github.com>
2024-11-14 11:59:29 +0000

    Created code for Part One A
    I think this is all thats needed for one A but please look at it to make sure the graphs are right because I might be graphing the wrong thing??

---
+++ Part1A.R
@@ -0,0 +1,43 @@
+# Function to simulate the temperature process and calculate P
+simulate_p <- function(t_increment, repeat_num) {
+  amount_of_increments <- 1 / t_increment   # Number of increments from 0 to 1
+  p_values <- numeric(repeat_num)  # To store proportion of
+  # time with positive temperature
+
+  # Monte Carlo simulation
+  for (i in 1:repeat_num) {
+    temperature <- numeric(amount_of_increments + 1)
+    for (j in 2:(amount_of_increments + 1)) {
+      temperature[j] <- temperature[j - 1] +
+        rnorm(1, mean = 0, sd = sqrt(t_increment))
+      # normal distribution with variance increment
+    }
+    # Calculate P for this number
+    p_values[i] <- mean(temperature > 0)  # Proportion of time with X(t) > 0
+  }
+
+  return(p_values)
+}
+
+# Parameters (change these)
+increments <- c(0.01, 0.001)  # Different t_increment values
+repeat_num <- 10000 # Number of times to simulate
+
+# Simulate for each t_increment and store results
+results <- list()
+for (t_increment in increments) {
+  cat("Simulating for t_increment =", t_increment, "\n")
+  p_values <- simulate_p(t_increment, repeat_num)
+  results[[as.character(t_increment)]] <- p_values
+}
+
+# Plotting the results for each t_increment
+par(mfrow = c(1, length(increments)))  # Set up a plot for each t_increment
+for (t_increment in increments) {
+  hist(results[[as.character(t_increment)]],
+       breaks = (1 / t_increment), probability = TRUE,
+       main = paste("Distribution of P (t_increment =", t_increment, ")"),
+       xlab = "Proportion of time with positive temperature",
+       ylab = "Frequency of Proportion P",
+       col = "#1d0fdf", border = "#000000")
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ProbabilityProject
 The study applies probability theory to understand how variables, such as temperature, evolve unpredictably over time, using assumptions like initial conditions and specific statistical characteristics for incremental changes.
+
+# Part1A
+This file uses monte carlo simulation to produce a graph for the proportion of temperatures above 0 for each time increment from [0, 1]. It can produce more than one graph based on the time increment and amount of times to run the simulation for a given time increment.
